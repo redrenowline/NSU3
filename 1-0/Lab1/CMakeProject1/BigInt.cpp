bc4be1f30dd67e8e6e748f2b4f39e7e3421140b4
redrenowline <redrenowline@gmail.com>
2022-10-30 17:10:17 +0700

    code1

---
+++ 1-0/Lab1/CMakeProject1/BigInt.cpp
@@ -0,0 +1,75 @@
+#include "BigInt.h"
+
+
+BigInt::BigInt() {
+	value.push_back(0);
+	sign = true;
+}
+
+
+BigInt::BigInt(int arg) {
+	value.push_back((unsigned int)abs(arg));
+	sign = arg >= 0 ? true : false;
+}
+
+BigInt::BigInt(std::string) {
+
+}
+
+BigInt::BigInt(const BigInt& arg) {
+	this->value = arg.value;
+	this->sign = arg.sign;
+
+}
+
+BigInt::~BigInt() {
+
+}
+
+BigInt& BigInt::operator+=(const BigInt& arg) {
+	BigInt cp(arg);
+	if (this->value.size() > cp.value.size()) cp.value.resize(this->value.size());
+	if (this->value.size() < cp.value.size()) this->value.resize(cp.value.size());
+	long long buffer = 0;
+	char overbuff = 0;
+	long long n = this->value.size();
+	for (int itr = 0; itr < n; itr++) {
+		buffer = (long long)this->value[itr] + (long long)(cp.sign == this->sign ? 1 : -1) * cp.value[itr];
+		buffer += (long long)overbuff;
+		overbuff = buffer / UINT_MAX;
+		this->value[itr] = (unsigned int) abs(buffer) % UINT_MAX;
+	}
+	if (buffer < 0) {
+		this->sign = !this->sign;
+	}
+	if (overbuff < 0) {
+		this->value.resize(this->value.size()+1);
+		this->value[this->value.size() - 1] = overbuff;
+	}
+	if (overbuff > 0) {
+		this->value.resize(this->value.size() + 1);
+		this->value[this->value.size() - 1] = overbuff;
+	}
+	return *this;
+}
+
+BigInt& BigInt::operator++() {
+	*this += BigInt(1);
+	return *this;
+}
+
+const BigInt BigInt::operator++(int) {
+	const BigInt cp = *this;
+	++(*this);
+	return cp;
+}
+
+BigInt& BigInt::operator--() {
+	*this += BigInt(-1);
+}
+
+const BigInt BigInt::operator--(int) {
+	const BigInt cp = *this;
+	--(*this);
+	return cp;
+}
--- 1-0/Lab1/CMakeProject1/main.cpp
+++ 1-0/Lab1/CMakeProject1/main.cpp
@@ -2,10 +2,17 @@
 #include <iostream>
 #include <gtest/gtest.h>
 
+#include "BigInt.h"
 
-TEST(testr, copyr) {
-	EXPECT_TRUE(5 == 5);
-}
-TEST(testr, copur) {
-	EXPECT_TRUE(7 < 10);
+//
+//TEST(basicConstructors, copy) {
+//	EXPECT_TRUE(5 == 5);
+//}
+//TEST(testr, copur) {
+//	EXPECT_TRUE(7 < 10);
+//}
+int main() {
+	BigInt bg1(INT_MAX);
+	BigInt bg2(INT_MAX);
+	++bg1;
 }
