a8658e9e98e5bf71f490b5428c16144cdf0683e3
redrenowline <redrenowline@gmail.com>
2022-12-15 09:16:16 +0700

    addtst2

--- 1-0/Lab1/CMakeProject1/main.cpp
+++ 1-0/Lab1/CMakeProject1/main.cpp
@@ -203,3 +203,61 @@ TEST(MainTests, t6){
   bg1 /= bg2;
   EXPECT_TRUE(std::string(bg1) == "1944378878");
 }
+
+TEST(MainTests, t7){
+  BigInt bg1("7123723819721");
+  BigInt bg2("633232111111");
+  bg1 /= bg2;
+  EXPECT_TRUE(std::string(bg1) == "11");
+}
+
+TEST(MainTests, t8){
+  BigInt bg1("7123723819721");
+  BigInt bg2("633232111111");
+  EXPECT_TRUE(bg1 != bg2);
+}
+
+TEST(MainTests, t9){
+  BigInt bg1("7123723819721");
+  BigInt bg2("633232111111");
+  EXPECT_FALSE(bg1 == bg2);
+}
+
+TEST(MainTests, t10){
+  BigInt bg1("7142141252");
+  BigInt bg2("123124124");
+  bg1 = bg1 * bg1 * bg2;
+  std::string strl = std::string(bg1);
+  EXPECT_TRUE(std::string(bg1) == "6280583931872964369492781916");
+}
+
+TEST(MainTests, t11){
+  BigInt bg1("8546654648484840");
+  BigInt bg2("7878798461236458");
+  BigInt bg3("7420746012508898");
+  bg1 &= bg2;
+  EXPECT_TRUE(bg1 == bg3);
+}
+
+TEST(MainTests, t12){
+  BigInt bg1("8546654648484840");
+  BigInt bg2("7878798461236458");
+  BigInt bg3("9004707097212400");
+  bg1 |= bg2;
+  EXPECT_TRUE(bg1 == bg3);
+}
+
+TEST(MainTests, t13){
+  BigInt bg1("8546654648484840");
+  BigInt bg2("7878798461236458");
+  BigInt bg3("1583961084703502");
+  bg1 ^= bg2;
+  EXPECT_TRUE(bg1 == bg3);
+}
+
+TEST(MainTests, t14){
+  BigInt bg1("8546654648484840");
+  BigInt bg2("18438197414766099480");
+  bg1 = ~bg1;
+  EXPECT_TRUE(bg1 == bg2);
+}
