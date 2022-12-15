5bb0026eb3dd14ad19378155d8d2573df6bfbe86
redrenowline <redrenowline@gmail.com>
2022-12-15 16:16:21 +0700

    delDirs

--- 1-0/build-Lab1-Desktop_x86_windows_msvc2019_pe_64bit-Release/CMakeProject1/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: D:/WORK/university2/1-0/NSU3/1-0/Lab1/CMakeProject1
-# Build directory: D:/WORK/university2/1-0/NSU3/1-0/build-Lab1-Desktop_x86_windows_msvc2019_pe_64bit-Release/CMakeProject1
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
