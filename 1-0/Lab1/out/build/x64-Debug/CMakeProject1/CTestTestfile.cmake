a603edc2ca8476918ad8fca24e5e255ebe09f71a
redrenowline <114148952+redrenowline@users.noreply.github.com>
2022-12-16 19:54:01 +0700

    Delete 1-0/Lab1/out directory

--- 1-0/Lab1/out/build/x64-Debug/CMakeProject1/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: D:/WORK/university2/1-0/NSU3/1-0/Lab1/CMakeProject1
-# Build directory: D:/WORK/university2/1-0/NSU3/1-0/Lab1/out/build/x64-Debug/CMakeProject1
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
