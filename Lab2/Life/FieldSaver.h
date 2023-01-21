24edf44205ba2b20c80e82daabff97f75d0d4c64
redrenowline <redrenowline@gmail.com>
2023-01-21 18:24:12 +0700

    add FieldSaver

---
+++ Lab2/Life/FieldSaver.h
@@ -0,0 +1,29 @@
+#ifndef FIELDSAVER_H
+#define FIELDSAVER_H
+
+#include <string>
+#include <iostream>
+#include <fstream>
+
+#include "Field.h"
+
+class FieldSaver{
+
+public:
+  FieldSaver();
+
+  void save(std::string path);
+
+  const char* INDICATOR = "#Life 1.06";
+  const char* NAMEINDICATOR = "#N";
+  const char* RULESINDICATOR = "#R";
+  const char* BIRTHINDICATOR = "#B";
+  const char* SURVIVEINDICATOR = "#S";
+
+private:
+
+protected:
+
+};
+
+#endif // FIELDSAVER_H
