974a8aa0b1c164fb7debbc667d66f3a0d5476a54
redrenowline <redrenowline@gmail.com>
2023-01-25 14:30:49 +0700

    Create TestCore class

---
+++ Lab2/Life/TestCore.cpp
@@ -0,0 +1,6 @@
+#include "TestCore.h"
+
+TestCore::TestCore()
+{
+
+}
---
+++ Lab2/Life/TestCore.h
@@ -0,0 +1,11 @@
+#ifndef TESTCORE_H
+#define TESTCORE_H
+
+
+class TestCore
+{
+public:
+  TestCore();
+};
+
+#endif // TESTCORE_H
